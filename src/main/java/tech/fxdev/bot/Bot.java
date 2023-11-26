d32d0aed04593a1ec68142a4abf7993768846243
Dmitrii Remiannikov <d.remiannikov@fxdev.tech>
2023-11-26 20:43:51 +0100

    Added new commands and small refactoring

--- src/main/java/tech/fxdev/bot/Bot.java
+++ src/main/java/tech/fxdev/bot/Bot.java
@@ -6,7 +6,13 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import tech.fxdev.bot.config.BotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,7 +21,26 @@ public class Bot extends TelegramLongPollingBot {
     final BotConfig config;
 
     public Bot(BotConfig config) {
+
         this.config = config;
+
+        List<BotCommand> listofCommands = new ArrayList<>();
+
+        listofCommands.add(new BotCommand("/start", "Dashboard"));
+        listofCommands.add(new BotCommand("/newthread", "Create a new thread"));
+        listofCommands.add(new BotCommand("/allthreads", "List of all your threads"));
+        listofCommands.add(new BotCommand("/subscription", "Manage your subscription"));
+        listofCommands.add(new BotCommand("/settings", "Manage your settings"));
+        listofCommands.add(new BotCommand("/help", "Help me"));
+
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+
+        }
+
     }
 
     @Override
@@ -40,6 +65,16 @@ public class Bot extends TelegramLongPollingBot {
                     startCommandReceived(chatId);
                     break;
 
+                case "/newthread":
+                    log.info("Received /newthread from User [" + update.getMessage().getFrom().getId() + "]");
+                    newthreadCommandReceived(chatId);
+                    break;
+
+                case "/allthreads":
+                    log.info("Received /allthreads from User [" + update.getMessage().getFrom().getId() + "]");
+                    allthreadsCommandReceived(chatId);
+                    break;
+
                 case "/subscription":
                     log.info("Received /subscription from User [" + update.getMessage().getFrom().getId() + "]");
                     subscriptionCommandReceived(chatId);
@@ -50,14 +85,9 @@ public class Bot extends TelegramLongPollingBot {
                     settingsCommandReceived(chatId);
                     break;
 
-                case "/newthread":
-                    log.info("Received /newthread from User [" + update.getMessage().getFrom().getId() + "]");
-                    newthreadCommandReceived(chatId);
-                    break;
-
-                case "/allthreads":
-                    log.info("Received /allthreads from User [" + update.getMessage().getFrom().getId() + "]");
-                    allthreadsCommandReceived(chatId);
+                case "/help":
+                    log.info("Received /help from User [" + update.getMessage().getFrom().getId() + "]");
+                    helpCommandReceived(chatId);
                     break;
 
                 default:
@@ -74,6 +104,18 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage(chatId, answer);
     }
 
+    private void newthreadCommandReceived(long chatId) {
+        String answer = "Threads section will be implemented in the future";
+
+        sendMessage(chatId, answer);
+    }
+
+    private void allthreadsCommandReceived(long chatId) {
+        String answer = "Chat section will be implemented in the future";
+
+        sendMessage(chatId, answer);
+    }
+
     private void subscriptionCommandReceived(long chatId) {
         String answer = "Subscription section will be implemented in the future";
 
@@ -86,14 +128,8 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage(chatId, answer);
     }
 
-    private void newthreadCommandReceived(long chatId) {
-        String answer = "Threads section will be implemented in the future";
-
-        sendMessage(chatId, answer);
-    }
-
-    private void allthreadsCommandReceived(long chatId) {
-        String answer = "Chat section will be implemented in the future";
+    private void helpCommandReceived(long chatId) {
+        String answer =  "You can execute commands from the main menu on the left or by typing a command:\n\n";
 
         sendMessage(chatId, answer);
     }
